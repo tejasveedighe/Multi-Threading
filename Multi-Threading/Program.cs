a957818113eceba9da3af0e7c55ed1d65ec62774
Tejasvee Dighe <tejasveedighe@gmail.com>
2024-03-05 12:32:44 +0530

    Add project files.

---
+++ Multi-Threading/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Multi_Threading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
